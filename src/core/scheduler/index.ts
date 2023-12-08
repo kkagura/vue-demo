82680152c9a1460f802207dae2d15307cc752b26
wangj <wangjianweb@sijibao.com>
2023-12-08 17:42:40 +0800

    feat: 修改nextTick的实现

--- src/core/scheduler/index.ts
+++ src/core/scheduler/index.ts
@@ -1,23 +1,23 @@
 let isFlushing = false;
 const resolvedPromise = Promise.resolve();
+let currentPromise: Promise<void> | null = null;
 const jobQueue = new Set<Function>();
 
 export function queueJob(job: Function) {
   jobQueue.add(job);
   if (isFlushing) return;
   isFlushing = true;
-  resolvedPromise
+  currentPromise = resolvedPromise
     .then(() => {
       jobQueue.forEach((job) => job());
     })
     .finally(() => {
       isFlushing = false;
       jobQueue.clear();
+      currentPromise = null;
     });
 }
 
 export function nextTick() {
-  return new Promise((resolve) => {
-    setTimeout(resolve);
-  });
+  return currentPromise || resolvedPromise;
 }
