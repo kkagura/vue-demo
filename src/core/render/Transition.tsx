7227987c98e5179699aff662f0427cacb0ee6694
wangj <wangjianweb@sijibao.com>
2023-12-12 19:08:54 +0800

    feat: Transition增加事件

--- src/core/render/Transition.tsx
+++ src/core/render/Transition.tsx
@@ -1,6 +1,6 @@
 import { RenderElement } from ".";
 import { nextFrame } from "../utils";
-import { Component, getCurrentInstance } from "./component";
+import { Component, getCurrentInstance, useEmit } from "./component";
 
 export interface TransitionHook {
   beforeEnter: (el: RenderElement) => void;
@@ -11,12 +11,26 @@ export interface TransitionHook {
 interface TransitionProps {
   name?: string;
   type?: "transition" | "animation";
+  onBeforeEnter?: (el: RenderElement) => void;
+  onEnter?: (el: RenderElement) => void;
+  onAfterEnter?: (el: RenderElement) => void;
+  onBeforeLeave?: (el: RenderElement) => void;
+  onLeave?: (el: RenderElement) => void;
+  onAfterLeave?: (el: RenderElement) => void;
 }
 
 const Transition: Component<TransitionProps> = (props) => {
   const getHookClass = (hookName: string) => {
     return (props.name || "") + "-" + hookName;
   };
+  const emit = useEmit<{
+    (e: "beforeEnter", el: RenderElement): void;
+    (e: "enter", el: RenderElement): void;
+    (e: "afterEnter", el: RenderElement): void;
+    (e: "beforeLeave", el: RenderElement): void;
+    (e: "leave", el: RenderElement): void;
+    (e: "afterLeave", el: RenderElement): void;
+  }>();
   return (context) => {
     const type = props.type || "transition";
     const endEventName = type + "end";
@@ -32,15 +46,18 @@ const Transition: Component<TransitionProps> = (props) => {
         beforeEnter(el) {
           el.classList.add(getHookClass("enter-from"));
           el.classList.add(getHookClass("enter-active"));
+          emit("beforeEnter", el);
         },
         enter(el) {
           nextFrame().then(() => {
             el.classList.remove(getHookClass("enter-from"));
             el.classList.add(getHookClass("enter-to"));
+            emit("enter", el);
             const afterEnter = () => {
               el.classList.remove(getHookClass("enter-active"));
               el.classList.remove(getHookClass("enter-to"));
               el.removeEventListener(endEventName, afterEnter);
+              emit("afterEnter", el);
             };
             el.addEventListener(endEventName, afterEnter);
           });
@@ -50,14 +67,17 @@ const Transition: Component<TransitionProps> = (props) => {
           el.classList.add(getHookClass("leave-active"));
           // 强制 reflow，使得初始状态生效
           document.body.offsetHeight;
+          emit("beforeLeave", el);
           nextFrame().then(() => {
             el.classList.remove(getHookClass("leave-from"));
             el.classList.add(getHookClass("leave-to"));
+            emit("leave", el);
             const afterLeave = () => {
               el.classList.remove(getHookClass("leave-active"));
               el.classList.remove(getHookClass("leave-to"));
               el.removeEventListener(endEventName, afterLeave);
               performanceRemove();
+              emit("afterLeave", el);
             };
             el.addEventListener(endEventName, afterLeave);
           });
